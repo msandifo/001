2b4cf64b5a3d63b2e5eecfca3075013c26ea8974
Mike Sandiford <msandifo@gmail.com>
2018-10-08 20:36:10 +1100

    init

--- drake001.R
+++ drake001.R
@@ -14,7 +14,7 @@ setwd(drake.path)
 download_aemo_aggregated(year=2010:2018, months=1:12, local.path=local.path)
 download_aemo_current( local.path=local.path )
 
-clean(twitter001)
+#clean(twitter001)
 make( twitter001)
 
 setwd("./figs")
